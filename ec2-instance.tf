fc5b8a4e67d2d7a905b05c47146629a75b2dc07a
unknown <arunreddyinfo@gmail.com>
2019-09-17 20:16:34 +0530

    5th commit for web content

--- ec2-instance.tf
+++ ec2-instance.tf
@@ -11,7 +11,8 @@ resource "aws_instance" "my-instance" {
   ami           = "ami-02913db388613c3e1"
   instance_type = "t2.micro"
   key_name = "${aws_key_pair.tf_demo.key_name}"
-
+  user_data = "${file("install-httpd.sh")}"
+   
   tags = {
     Name = "Terraform"
   }
